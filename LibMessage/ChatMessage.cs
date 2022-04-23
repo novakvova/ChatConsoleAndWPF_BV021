86fb92ce21a8e4689e1c760e846a0d63f1551864
novakvova <novakvova@gmail.com>
2022-04-23 18:47:59 +0300

    Good chat

---
+++ LibMessage/ChatMessage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace LibMessage
+{
+    public enum TypeMessage
+    {
+        Login,
+        Logout,
+        Message
+    }
+    public class ChatMessage
+    {
+        public TypeMessage MessageType;
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Text { get; set; }
+
+        public byte[] Serialize()
+        {
+            using (MemoryStream m = new MemoryStream())
+            {
+                using (BinaryWriter writer = new BinaryWriter(m))
+                {
+                    writer.Write((int)MessageType);
+                    writer.Write(UserId);
+                    writer.Write(UserName);
+                    writer.Write(Text);
+                }
+                return m.ToArray();
+            }
+        }
+
+        public static ChatMessage Desserialize(byte[] data)
+        {
+            ChatMessage result = new ChatMessage();
+            using (MemoryStream m = new MemoryStream(data))
+            {
+                using (BinaryReader reader = new BinaryReader(m))
+                {
+                    result.MessageType = (TypeMessage)reader.ReadInt32();
+                    result.UserId = reader.ReadString();
+                    result.UserName = reader.ReadString();
+                    result.Text = reader.ReadString();
+                }
+            }
+            return result;
+        }
+    }
+}
--- WpfClient/MainWindow.xaml.cs
+++ WpfClient/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LibMessage;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -29,6 +31,7 @@ namespace WpfClient
         TcpClient client = new TcpClient();
         NetworkStream ns;
         Thread thread;
+        private ChatMessage _message = new ChatMessage(); 
 
 
         public MainWindow()
@@ -51,48 +54,98 @@ namespace WpfClient
                         port = int.Parse(sr.ReadLine());
                     }
                 }
-
+                _message.UserName = txtUserName.Text;
+                _message.UserId = Guid.NewGuid().ToString();
                 client.Connect(ip, port);
-                lbInfo.Items.Add("Connect to server "+ ip.ToString()+":"+port);
+                lbInfo.Items.Add("Підключення до сервера "+ ip.ToString()+":"+port);
                 ns = client.GetStream();
                 thread = new Thread(o => ReceiveData((TcpClient)o));
                 thread.Start(client);
 
+                _message.MessageType = TypeMessage.Login;
+                _message.Text = "Приєднався до чату";
+                byte[] buffer = _message.Serialize(); //Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(_message));
+                ns.Write(buffer, 0, buffer.Length);
             }
             catch(Exception ex)
             {
-                MessageBox.Show("Problem Connection Server");
+                MessageBox.Show("Problem Connection Server "+ ex.Message);
             }
 
         }
 
         private void bntSend_Click(object sender, RoutedEventArgs e)
         {
-            byte[] buffer = Encoding.UTF8.GetBytes(txtText.Text);
+            _message.MessageType = TypeMessage.Message;
+            _message.Text = txtText.Text;
+            byte[] buffer = _message.Serialize(); //Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(_message));
             ns.Write(buffer, 0, buffer.Length);
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+            _message.MessageType = TypeMessage.Logout;
+            _message.Text = "Покинув чат";
+            byte[] buffer = _message.Serialize(); //Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(_message));
+            ns.Write(buffer, 0, buffer.Length);
+
             client.Client.Shutdown(SocketShutdown.Send);
             thread.Join();
             ns.Close();
             client.Close();
-            Console.WriteLine("disconnect from server!!");
+            //Console.WriteLine("disconnect from server!!");
         }
         private void ReceiveData(TcpClient client)
         {
             NetworkStream ns = client.GetStream();
             byte[] receivedBytes = new byte[1024];
             int byte_count;
-
+            string data="";
             while ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
             {
                 Dispatcher.BeginInvoke(new Action(() =>
                 {
-                        lbInfo.Items.Add(Encoding.UTF8.GetString(receivedBytes, 0, byte_count));
+                    try
+                    {
+
+                        ChatMessage message = ChatMessage.Desserialize(receivedBytes);
+                        switch (message.MessageType)
+                        {
+                            case TypeMessage.Login:
+                                {
+                                    if (message.UserId != _message.UserId)
+                                        lbInfo.Items.Add(message.UserName + ":" + message.Text);
+                                    break;
+
+                                }
+                            case TypeMessage.Logout:
+                                {
+                                    if (message.UserId != _message.UserId)
+                                        lbInfo.Items.Add(message.UserName + ":" + message.Text);
+                                    break;
+
+                                }
+                            case TypeMessage.Message:
+                                {
+                                    lbInfo.Items.Add(message.UserName + ":" + message.Text);
+                                    break;
+
+                                }
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+
+                        MessageBox.Show("Deserialize object problem " + ex.Message);
+                    }
+
                 }));
+                //data += Encoding.UTF8.GetString(receivedBytes, 0, byte_count);
+
             }
+            
+           
         }
 
         
